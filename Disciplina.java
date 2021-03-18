9a02a05d15dbdff33ee2152e952ec3699e3b4fa2
Carolina-Batiston <80919505+Carolina-Batiston@users.noreply.github.com>
2021-03-18 16:24:49 -0300

    Envio classe Disciplina

---
+++ Disciplina.java
@@ -0,0 +1,102 @@
+public class Disciplina {
+	private String nomeDisciplina;
+	private int cargaHoraria;
+	private String ementa;
+	private int idDisciplina;
+	private ArrayList<File> documentos;
+	private Tutor tutor;
+	private ArrayList<Avaliacao> avaliacoes;
+	private Turma turma;
+	
+	public String getNomeDisciplina() {
+		return nomeDisciplina;
+	}
+	public void setNomeDisciplina(String nomeDisciplina) {
+		this.nomeDisciplina = nomeDisciplina;
+	}
+	public int getCargaHoraria() {
+		return cargaHoraria;
+	}
+	public void setCargaHoraria(int cargaHoraria) {
+		this.cargaHoraria = cargaHoraria;
+	}
+	public String getEmenta() {
+		return ementa;
+	}
+	public void setEmenta(String ementa) {
+		this.ementa = ementa;
+	}
+	public int getIdDisciplina() {
+		return idDisciplina;
+	}
+	public void setIdDisciplina(int idDisciplina) {
+		this.idDisciplina = idDisciplina;
+	}
+	
+	public File getDocumentos() {
+		return documentos;
+	}
+	public void setDocumentos(ArrayList<File> documentos) {
+		this.documentos = documentos;
+	}
+	public uploadMaterialMidia(File f) {
+		
+	}
+	
+	public editarMaterialMidia(int i) {
+		
+	}
+	
+	public removerMaterialMidia(int i) {
+		
+	}
+	
+	public exibirAtividades() {
+		
+	}
+	
+	public relatorioBoletim(Aluno aluno) {
+		
+	}
+	
+	public visualizarAtividades() {
+		
+	}
+	
+	public adicionarAvaliacao(Avaliacao a) {
+		
+	}
+	
+	public editarAvaliacao(Avaliacao a) {
+		
+	}
+	
+	public boolean removerAvaliacao(Avaliacao a) {
+		
+	}
+	
+	public cadastrarTurma(Turma t) {
+		
+	}
+	
+	public editarTurma(Turma t) {
+		
+	}
+	
+	public boolean removerTurma(Turma t) {
+		
+	}
+	
+	public adicionarTutor(Tutor tutor) {
+		
+	}
+	
+	public editarTutor(Tutor tutor) {
+		
+	}
+	
+	public removerTutor(Tutor tutor) {
+		
+	}
+	
+}
