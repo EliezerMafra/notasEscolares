578e27c4aa5f55ec1b2707ec8f6cb55141703faf
Matheus Silveira <62355040+math-silveira@users.noreply.github.com>
2021-03-18 16:24:44 -0300

    envio da classe turma

---
+++ Turma.java
@@ -0,0 +1,64 @@
+package projetoStraus;
+
+    public class Turma {
+
+        private int codigoTurma;
+        private int frequencias[];
+        private String nomeTurma;
+        private String turnoTurma;
+        private double notasAluno;
+
+        public Turma(){}
+
+        public int getCodigoTurma() {
+            return codigoTurma;
+        }
+
+        public void setCodigoTurma(int codigoTurma) {
+            this.codigoTurma = codigoTurma;
+        }
+
+        public int[] getFrequencias() {
+            return frequencias;
+        }
+
+        public void setFrequencias(int[] frequencias) {
+            this.frequencias = frequencias;
+        }
+
+        public String getNomeTurma() {
+            return nomeTurma;
+        }
+
+        public void setNomeTurma(String nomeTurma) {
+            this.nomeTurma = nomeTurma;
+        }
+
+        public String getTurnoTurma() {
+            return turnoTurma;
+        }
+
+        public void setTurnoTurma(String turnoTurma) {
+            this.turnoTurma = turnoTurma;
+        }
+
+        public double getNotasAluno() {
+            return notasAluno;
+        }
+
+        public void setNotasAluno(double notasAluno) {
+            this.notasAluno = notasAluno;
+        }
+
+        public void adicionarAluno(Aluno) {}
+
+        public void editarAluno(Aluno) {}
+
+        public void removerAluno(Aluno) {
+        	
+        }
+
+
+    }
+    
+	}
